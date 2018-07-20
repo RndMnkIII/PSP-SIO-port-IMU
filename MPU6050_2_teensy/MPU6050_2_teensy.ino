27faf12d2698de450c0cfb11a9f8adcc2dbf2e90
RndMnkIII <rndmnkiii@gmail.com>
2018-07-20 21:22:19 +0200

    Add files via upload

---
+++ MPU6050_2_teensy/MPU6050_2_teensy.ino
@@ -0,0 +1,209 @@
+/* RndMnkIII 2018
+ *  Example using MPU-6050 sensor as IMU thanks to the Kalman filter library
+ *  by Kristian Lauszus, to convert the raw data from the sensor to float values
+ *  representing the x and y axis rotation values. This values are converted to a
+ *  byte array and transfered to the PSP SIO port using the Teensy 3.2 UART2 at 9600bps.
+ *  The UART2 port is connected to the PSP console SIO port using a bidirectional
+ *  voltage level converter between for converting from 3.3v Teensy 3.2 logic level
+ *  to the PSP SIO 2.5v logic level using a sparkfun logic level shifter.
+ *  (https://www.sparkfun.com/products/12009).
+ *  
+ *  Connection diagram:
+ *   -------------------------           ------------------   -------------------------   -------------------
+ *  /    |_|    |_|    |_|    \          |   sparkfun     |   |      Teensy 3.2       |   |     MPU-6050    |      
+ *  |     1 MIC  2 GND  3 TX   | -- 3 -- |LV1  level  HV1 |---| 9  RX UART2    SCL0 19|---| SCL             |
+ *  |       PSP SIO Port       | -- 6 -- |LV2 shifter HV2 |---| 10 TX UART2    SDA0 18|---| SDA             |
+ *  |     4 ?   5 +2.5V 6 RX   | -- 5 -- |LV           HV |---| +3.3v           +3.3v |---| +3.3V           |
+ *  |     _      _      _      | -- 2 -- |GND         GND |---| GND               GND |---| GND             |
+ *  \    | |    | |    | |    /          |                |   |                       |   |                 |
+ *   -------------------------           ------------------   -------------------------   -------------------
+
+GNU General Public License v3.0 (see LICENSE)
+ */
+
+#include <Wire.h>
+#include <Kalman.h> // Source author Kristian Lauszus: https://github.com/TKJElectronics/KalmanFilter
+
+//#define RESTRICT_PITCH // Comment out to restrict roll to ±90deg instead - please read: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf
+
+#define HWSERIAL Serial2 //TX:10 RX: 9 Teensy 3.2
+
+//to convert from float values to byte array
+union floatAsBytes{
+    float fval;
+    byte bval[4];
+};
+
+
+union floatAsBytes fltnbX;
+union floatAsBytes fltnbY;
+
+//write the floats to UART as a byte sequence
+void enviaFloat(union floatAsBytes valor)
+{
+  HWSERIAL.write(valor.bval[0]);
+  HWSERIAL.write(valor.bval[1]);
+  HWSERIAL.write(valor.bval[2]);
+  HWSERIAL.write(valor.bval[3]);
+}
+
+Kalman kalmanX; // Create the Kalman instances
+Kalman kalmanY;
+
+/* IMU Data */
+double accX, accY, accZ;
+double gyroX, gyroY, gyroZ;
+int16_t tempRaw;
+
+double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+double compAngleX, compAngleY; // Calculated angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+
+uint32_t timer;
+uint8_t i2cData[14]; // Buffer for I2C data
+
+// TODO: Make calibration routine
+
+void setup() {
+  //Serial.begin(9600);
+  HWSERIAL.begin(9600);
+  Wire.begin();
+#if ARDUINO >= 157
+  Wire.setClock(400000UL); // Set I2C frequency to 400kHz
+#else
+  TWBR = ((F_CPU / 400000UL) - 16) / 2; // Set I2C frequency to 400kHz
+#endif
+
+  i2cData[0] = 7; // Set the sample rate to 1000Hz - 8kHz/(7+1) = 1000Hz
+  i2cData[1] = 0x00; // Disable FSYNC and set 260 Hz Acc filtering, 256 Hz Gyro filtering, 8 KHz sampling
+  i2cData[2] = 0x00; // Set Gyro Full Scale Range to ±250deg/s
+  i2cData[3] = 0x00; // Set Accelerometer Full Scale Range to ±2g
+  while (i2cWrite(0x19, i2cData, 4, false)); // Write to all four registers at once
+  while (i2cWrite(0x6B, 0x01, true)); // PLL with X axis gyroscope reference and disable sleep mode
+
+  while (i2cRead(0x75, i2cData, 1));
+  if (i2cData[0] != 0x68) { // Read "WHO_AM_I" register
+    //HWSERIAL.print(F("Error reading sensor"));
+    while (1);
+  }
+
+  delay(100); // Wait for sensor to stabilize
+
+  /* Set kalman and gyro starting angle */
+  while (i2cRead(0x3B, i2cData, 6));
+  accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
+  accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
+  accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
+
+  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // It is then converted from radians to degrees
+#ifdef RESTRICT_PITCH // Eq. 25 and 26
+  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+
+  kalmanX.setAngle(roll); // Set starting angle
+  kalmanY.setAngle(pitch);
+  gyroXangle = roll;
+  gyroYangle = pitch;
+  compAngleX = roll;
+  compAngleY = pitch;
+
+  timer = micros();
+}
+
+void loop() {
+  /* Update all the values */
+  while (i2cRead(0x3B, i2cData, 14));
+  accX = (int16_t)((i2cData[0] << 8) | i2cData[1]);
+  accY = (int16_t)((i2cData[2] << 8) | i2cData[3]);
+  accZ = (int16_t)((i2cData[4] << 8) | i2cData[5]);
+  tempRaw = (int16_t)((i2cData[6] << 8) | i2cData[7]);
+  gyroX = (int16_t)((i2cData[8] << 8) | i2cData[9]);
+  gyroY = (int16_t)((i2cData[10] << 8) | i2cData[11]);
+  gyroZ = (int16_t)((i2cData[12] << 8) | i2cData[13]);;
+
+  double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
+  timer = micros();
+
+  // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
+  // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
+  // It is then converted from radians to degrees
+#ifdef RESTRICT_PITCH // Eq. 25 and 26
+  double roll  = atan2(accY, accZ) * RAD_TO_DEG;
+  double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
+#else // Eq. 28 and 29
+  double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
+  double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
+#endif
+
+  double gyroXrate = gyroX / 131.0; // Convert to deg/s
+  double gyroYrate = gyroY / 131.0; // Convert to deg/s
+
+#ifdef RESTRICT_PITCH
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
+    kalmanX.setAngle(roll);
+    compAngleX = roll;
+    kalAngleX = roll;
+    gyroXangle = roll;
+  } else
+    kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+
+  if (abs(kalAngleX) > 90)
+    gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
+#else
+  // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+  if ((pitch < -90 && kalAngleY > 90) || (pitch > 90 && kalAngleY < -90)) {
+    kalmanY.setAngle(pitch);
+    compAngleY = pitch;
+    kalAngleY = pitch;
+    gyroYangle = pitch;
+  } else
+    kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt); // Calculate the angle using a Kalman filter
+
+  if (abs(kalAngleY) > 90)
+    gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
+  kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+#endif
+
+  gyroXangle += gyroXrate * dt; // Calculate gyro angle without any filter
+  gyroYangle += gyroYrate * dt;
+  //gyroXangle += kalmanX.getRate() * dt; // Calculate gyro angle using the unbiased rate
+  //gyroYangle += kalmanY.getRate() * dt;
+
+  compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll; // Calculate the angle using a Complimentary filter
+  compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
+
+  // Reset the gyro angle when it has drifted too much
+  if (gyroXangle < -180 || gyroXangle > 180)
+    gyroXangle = kalAngleX;
+  if (gyroYangle < -180 || gyroYangle > 180)
+    gyroYangle = kalAngleY;
+
+  fltnbX.fval = (float) kalAngleX;
+  fltnbY.fval = (float) kalAngleY;
+
+  HWSERIAL.write(0xD); //añadimos un retorno de carro para detectar inicio de envio de datos
+  enviaFloat(fltnbX);
+  enviaFloat(fltnbY);
+  
+  Serial.print(fltnbX.fval);
+  Serial.print("\t");
+  Serial.print(fltnbY.fval);
+  Serial.print("\n");
+  
+  
+  //enviaFloat(fltnbY);
+
+  delay(16); //to limit the frequency data rate to 60Hz.
+}
+
+
+
+
